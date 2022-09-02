5d527d2eaadf9ac4a1b88c04e1750e236a257d57
JEONGHWANMIN <mimi0j0j@naver.com>
2022-09-02 15:22:46 +0900

    feat : add jsdoc

---
+++ src/index.js
@@ -0,0 +1,51 @@
+/*
+필수 요구사항
+아래의 Todo 앱 요구사항을 참고하여
+
+필요한 데이터를 모두 모델링한다.
+사용되는 모든 함수를 선언부만 만든다.
+함수 및 클래스의 내부는 구현하지 않습니다.
+JSDoc을 활용해 문서화한다.
+GitHub Page를 활용해 JSDoc 정적 페이지를 배포한다.
+
+
+Todo {
+  아이디(required),
+  내용(required),
+  완료여부(required),
+  카테고리(required),
+  태그들(optional),
+}
+
+*/
+
+/**
+ * 할일
+ * @typedef {Object} Todo
+ * @property {number} id - 할일 id
+ * @property {string} content - 할일 내용
+ * @property {boolean} isDone - 할일 완료 여부
+ * @property {string} category - 할일 카테고리
+ * @property {string[]} [tags] - The tags of the todo.
+ */
+
+/**
+ * Represents a todo.
+ * @param {number} id - The id of the todo.
+ * @param {string} content - The content of the todo.
+ * @param {boolean} isDone - The isDone of the todo.
+ * @param {string} category - The category of the todo.
+ * @param {string[]} [tags] - The tags of the todo.
+ *
+ */
+function CREATE(id, content, isDone, category, tags) {}
+
+/**
+ * Represents a todo.
+ * @param {number} [id] - The id of the todo.
+ * @returns {Todo|Todo[]} if id in Param return Todo , else return Todos
+ */
+function READ() {}
+
+function UPDATE(content, isDone, category, tags) {}
+function DELETE() {}
